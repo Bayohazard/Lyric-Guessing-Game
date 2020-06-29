e276b91516dce9d0b781c7daa13d18cb83e1b978
Bryan Paez <bryanpaez2014@yahoo.com>
2020-06-28 23:19:24 -0700

    Make restarting database optional

--- index.php
+++ index.php
@@ -9,11 +9,14 @@
   <body onload="load()">
 
     <?php
+
     $servername = "localhost";
     $username = "root";
     $password = "";
     $database_name = "song_information";
 
+    // Set to true to clear and refill the databse
+    $should_restart_database = False;
 
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $database_name);
@@ -23,40 +26,50 @@
       die('Could not connect: ' . mysqli_error($conn));
     }
 
-    // Preparing statement
-    $statement = mysqli_prepare($conn, "INSERT INTO songs
-            (Title, Length, Artist, Album, Genre, Is_Loved, Plays)
-            VALUES (?, ?, ?, ?, ?, ?, ?)");
 
-    // Binding values to prepared statement(Prevents SQL injection attacks)
-    $statement->bind_param("sssssii", $title, $length, $artist, $album,
-            $genre, $is_loved, $plays);
-            
-    // Clears the table
-    $sql = "DELETE FROM songs";
-    mysqli_query($conn, $sql);
 
-    // Open file in "read" mode
-    $file = fopen("song-information.txt", "r");
+    function restart_database($connection) {
+      // Clears the table
+      $sql = "DELETE FROM songs";
+      mysqli_query($connection, $sql);
 
-    while(!feof($file)) {
-      $line = fgets($file);
-      $arr = explode("\t", $line);
-      $title = $arr[0];
-      $length = $arr[2];
-      $artist = $arr[3];
-      $album = $arr[4];
-      $genre = $arr[5];
-      $is_loved = $arr[6] == 0 ? 0 : 1;
-      $plays = isset($arr[7]) ? intval($arr[7]) : 0;
+      // Preparing statement
+      $statement = mysqli_prepare($connection, "INSERT INTO songs
+              (Title, Length, Artist, Album, Genre, Is_Loved, Plays)
+              VALUES (?, ?, ?, ?, ?, ?, ?)");
 
-      // Execute the prepared statement after updating the variables
-      $statement->execute();
+      // Binding values to prepared statement(Prevents SQL injection attacks)
+      $statement->bind_param("sssssii", $title, $length, $artist, $album,
+              $genre, $is_loved, $plays);
+
+      // Open file in "read" mode
+      $file = fopen("song-information.txt", "r");
+
+      // Parses each line and assigns variables
+      while(!feof($file)) {
+        $line = fgets($file);
+        $arr = explode("\t", $line);
+        $title = $arr[0];
+        $length = $arr[2];
+        $artist = $arr[3];
+        $album = $arr[4];
+        $genre = $arr[5];
+        $is_loved = $arr[6] == 0 ? 0 : 1;
+        $plays = isset($arr[7]) ? intval($arr[7]) : 0;
+
+        // Execute the prepared statement after updating the variables
+        $statement->execute();
+      }
     }
+
+    if($should_restart_database) {
+      restart_database($conn);
+    }
+
     mysqli_close($conn);
     ?>
 
-    <h1 id="title">Finish The Lyric</h1>
+    <h1 id="title">Song Quiz</h1>
     <script type="text/javascript" src="script.js"></script>
   </body>
 </html>
